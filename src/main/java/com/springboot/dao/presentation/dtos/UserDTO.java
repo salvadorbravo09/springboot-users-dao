8fe9868555bfa66e2333437f5ba8b541b56f8298
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-08 18:33:30 -0400

    se creo la clase dto para la entidad de user

---
+++ src/main/java/com/springboot/dao/presentation/dtos/UserDTO.java
@@ -0,0 +1,17 @@
+package com.springboot.dao.presentation.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String lastName;
+    private String email;
+    private byte age;
+}
