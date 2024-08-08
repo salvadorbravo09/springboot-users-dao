603584ee7942aa889ca46642341a9117a18a8585
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-08 18:32:30 -0400

    se creo la entidad principal de user con sus propiedades

---
+++ src/main/java/com/springboot/dao/persistence/entities/UserEntity.java
@@ -0,0 +1,26 @@
+package com.springboot.dao.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+    private byte age;
+}
