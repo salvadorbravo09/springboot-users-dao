439b1828f52cc41c2c4a6d4961019715704cbbad
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-08-08 16:54:55 -0400

    initial commit

---
+++ src/test/java/com/springboot/dao/SpringbootDaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.dao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
